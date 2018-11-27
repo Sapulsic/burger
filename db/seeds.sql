42acfea5f224c94608b2a584287d91dcaa23d94c
Sapulsic <carlos.barahona1995@gmail.com>
2018-11-27 17:01:15 -0600

    Base files

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Shroom');
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('El Diablo');
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Impossible', true);
